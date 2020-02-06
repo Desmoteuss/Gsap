646cd58467025a417c5ead27553ae3c238cf1a4e
Desmoteuss <desmoteuss@gmail.com>
2020-02-06 22:11:38 +0100

    logo and animations added

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const headline = document.querySelector(".headline");
 
 const tl = new TimelineMax();
 
-tl.fromTo(back, 1, { height: "10%" }, { height: "80%" }).fromTo(
-  slider,
-  1.2,
-  { x: "-100%" },
-  { x: "0%" }
-);
+tl.fromTo(back, 1, { height: "0%" }, { height: "80%" })
+  .fromTo(back, 1.2, { width: "100%" }, { width: "80%" })
+  .fromTo(slider, 1.3, { x: "-100%" }, { x: "0%" })
+  .fromTo(logo, 1.4, { opacity: "0" }, { opacity: "1" })
+  .fromTo(headline, 1.5, { opacity: "0" }, { opacity: "1", y: 20, x: 10 })
+
